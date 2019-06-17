36ad1a55a1e0054d8219632be0d19c6e42ad152f
JackXuZhaoLong <39019494+JackXuZhaoLong@users.noreply.github.com>
2019-06-17 16:16:08 +0800

    Update functions.js

--- css/functions.js
+++ css/functions.js
@@ -40,7 +40,7 @@ $(window).resize(function() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+	var days = Math.floor(seconds / (3600 * 24))+31;
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
